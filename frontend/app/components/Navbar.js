b77fd195a5b886e17ccf2a345c3da83609c8511d
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2025-02-14 15:35:39 +0800

    fix: change  taglione

--- frontend/app/components/Navbar.js
+++ frontend/app/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <div className="p-4">
             <h2 className=" text-4xl font-bold">Grammr 💬</h2>
-            <p>Correct your sentence powered by AI</p>
+            <p>Correct your sentences with AI.</p>
         </div>
     )
 }
